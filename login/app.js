d91a4a89428f3688d8550be0e7bfd8ec2e29f292
MatheusVinicius-pazz <mvpr1@aluno.ifal.edu.br>
2025-07-17 10:59:50 -0300

    NEW

--- login/app.js
+++ login/app.js
@@ -7,15 +7,15 @@ document.getElementById('login-form').addEventListener('submit', function(event)
     const password = document.getElementById('password').value;
 
     // Dados fictícios para login
-    const validUsername = "usuario123";
-    const validPassword = "senha123";
+    const validUsername = "admin";
+    const validPassword = "1234";
 
-    // Verificação de login
-    if (username === validUsername && password === validPassword) {
-        alert("Login bem-sucedido!");
-        // Aqui você pode redirecionar para uma página interna, por exemplo:
-        // window.location.href = 'pagina-principal.html';
-    } else {
-        document.getElementById('error-message').style.display = "block";
-    }
+     // Simulação de validação (substituir com sua lógica real)
+     if (username === 'admin' && password === '1234') {
+        // Login bem-sucedido -> redireciona
+        window.location.href = '../index.html';
+      } else {
+        alert('Usuário ou senha incorretos!');
+      }
 });
+
